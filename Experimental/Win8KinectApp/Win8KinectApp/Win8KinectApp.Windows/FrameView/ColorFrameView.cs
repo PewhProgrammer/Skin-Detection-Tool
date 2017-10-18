ba65862f45689889a6378863c8de44ef387f4129
PewhProgrammer <vlukiray@yahoo.com>
2017-10-18 14:10:29 +0200

    added colorFrame

--- Experimental/Win8KinectApp/Win8KinectApp/Win8KinectApp.Windows/FrameView/ColorFrameView.cs
+++ Experimental/Win8KinectApp/Win8KinectApp/Win8KinectApp.Windows/FrameView/ColorFrameView.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
-namespace Win8KinectApp.FrameView
+namespace Win8KinectApp.FrameKinectView
 {
-    class ColorFrameView
+    class ColorFrameView : FrameView
     {
+        public override void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
